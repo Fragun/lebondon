e38e252c85c82a38197ace4084531d3dde398b11
[Dondede] <[dethoordavid@gmail.com]>
2022-08-03 23:51:37 +0200

    ajout lien header

--- src/Entity/Departement.php
+++ src/Entity/Departement.php
@@ -21,12 +21,28 @@ class Departement
      */
     private $idDepartement;
 
+        /**
+     * @var int
+     *
+     * @ORM\Column(name="CODE_DEPARTEMENT", type="integer", nullable=false)
+     */
+    private $codeDepartement;
+
     /**
      * @var string
      *
      * @ORM\Column(name="NOM_DEPARTEMENT", type="string", length=100, nullable=false)
      */
     private $nomDepartement;
+
+        /**
+     * @var string
+     *
+     * @ORM\Column(name="SLUG_DEPARTEMENT", type="string", length=150, nullable=false)
+     */
+    private $slugDepartement;
+    
+
 
     public function getIdDepartement(): ?int
     {
@@ -46,4 +62,52 @@ class Departement
     }
 
 
+
+    /**
+     * Get the value of codeDepartement
+     *
+     * @return  int
+     */ 
+    public function getCodeDepartement()
+    {
+        return $this->codeDepartement;
+    }
+
+    /**
+     * Set the value of codeDepartement
+     *
+     * @param  int  $codeDepartement
+     *
+     * @return  self
+     */ 
+    public function setCodeDepartement(int $codeDepartement)
+    {
+        $this->codeDepartement = $codeDepartement;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of slugDepartement
+     *
+     * @return  string
+     */ 
+    public function getSlugDepartement()
+    {
+        return $this->slugDepartement;
+    }
+
+    /**
+     * Set the value of slugDepartement
+     *
+     * @param  string  $slugDepartement
+     *
+     * @return  self
+     */ 
+    public function setSlugDepartement(string $slugDepartement)
+    {
+        $this->slugDepartement = $slugDepartement;
+
+        return $this;
+    }
 }
